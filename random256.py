fe30afec2ef9d46300c06f6a8ce44c9539e404a0
5ak1r <s.azimkar1@newcastle.ac.uk>
2024-05-05 16:20:20 +0100

    doc link added

--- random256.py
+++ random256.py
@@ -2,3 +2,4 @@ print(__import__('random').sample(range(1, 257), 256))
 
 
 #https://www.youtube.com/watch?v=U3sT-T5bKX4
+#https://docs.google.com/spreadsheets/d/1tNecQ3MNpCZDer70nFl5YaF2da-zKOk5yubNMsi0xQQ/edit?usp=sharing
