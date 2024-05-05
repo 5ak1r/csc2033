70b1f31095b44f9d5aeeb39ac811e2c94fcf62ba
5ak1r <s.azimkar1@newcastle.ac.uk>
2024-05-05 21:33:05 +0100

    added another link

--- random256.py
+++ random256.py
@@ -1,5 +1,6 @@
 print(__import__('random').sample(range(1, 257), 256))
 
 
-#https://www.youtube.com/watch?v=U3sT-T5bKX4
-#https://docs.google.com/spreadsheets/d/1tNecQ3MNpCZDer70nFl5YaF2da-zKOk5yubNMsi0xQQ/edit?usp=sharing
+#https://www.youtube.com/watch?v=U3sT-T5bKX4 //sidescroller tutorial
+#https://docs.google.com/spreadsheets/d/1tNecQ3MNpCZDer70nFl5YaF2da-zKOk5yubNMsi0xQQ/edit?usp=sharing //our google doc
+#https://www.youtube.com/watch?v=Ldyw5IFkEUQ& //procedural generation
